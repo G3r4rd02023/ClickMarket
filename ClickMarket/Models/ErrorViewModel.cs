062ee36b49d563c0cfd54ca2c6744a1e98ee65eb
Laly Durón <LalyDOsorto@gmail.com>
2022-03-09 19:53:38 -0600

    Creacion del Proyecto

---
+++ ClickMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClickMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
